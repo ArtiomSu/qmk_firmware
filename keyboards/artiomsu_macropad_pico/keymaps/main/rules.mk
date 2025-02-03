8416880081e5a06e3615844a113e0ecca11818ac
ArtiomSu <doasartiom@gmail.com>
2025-02-03 15:13:03 +0000

    update config to use new json format

--- keyboards/artiomsu_macropad_pico/keymaps/main/rules.mk
+++ keyboards/artiomsu_macropad_pico/keymaps/main/rules.mk
@@ -1,7 +1,7 @@
 
 
-LEADER_ENABLE = yes
-PROGRAMMABLE_BUTTON_ENABLE = yes
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+#LEADER_ENABLE = yes
+#PROGRAMMABLE_BUTTON_ENABLE = yes
+#EXTRAKEY_ENABLE = yes       # Audio control and System control
 
 SRC+= calculator.c dtostrf.c macros.c led_control.c
--- keyboards/artiomsu_macropad_pico/rules.mk
+++ keyboards/artiomsu_macropad_pico/rules.mk
@@ -1,17 +1,18 @@
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-RGBLIGHT_DRIVER = ws2812
-WS2812_DRIVER = vendor
-AUDIO_ENABLE = no           # Audio output
-SPLIT_KEYBOARD = no
+#BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+#MOUSEKEY_ENABLE = no       # Mouse keys
+#EXTRAKEY_ENABLE = no       # Audio control and System control
+#CONSOLE_ENABLE = no         # Console for debug
+#COMMAND_ENABLE = no        # Commands for debug and configuration
+#NKRO_ENABLE = no            # Enable N-Key Rollover
+#BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+#RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+#RGBLIGHT_DRIVER = ws2812
+#WS2812_DRIVER = vendor
+#AUDIO_ENABLE = no           # Audio output
+#SPLIT_KEYBOARD = no
+#ENCODER_ENABLE = yes
+#OLED_ENABLE = yes
 
-OLED_ENABLE = yes
+# this is to use the I2C driver for oled display. Not sure if its still needed
 OPT_DEFS += -DHAL_USE_I2C=TRUE
 
-ENCODER_ENABLE = yes
